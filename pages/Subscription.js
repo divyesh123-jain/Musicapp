d0d603de61dd310215919fb35b8bbc6635861029
divyesh123-jain <97737499+divyesh123-jain@users.noreply.github.com>
2023-06-29 09:57:37 +0530

    update

--- pages/Subscription.js
+++ pages/Subscription.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Subscription = () => {
   return (
     <>
-      {/* subscription cards */}
+      {/* subscription cards
       <div class="flex-1  justify-center items-center w-[256px] px-2 sm:px-0">
         <div class="flex justify-center items-center w-[756px]">
-               {/* <h3 class=" font-bold mt-12 text-4xl">All Artists</h3> */}
+               <h3 class=" font-bold mt-12 text-4xl">All Artists</h3>
         </div>
-        </div>
+        </div> */}
     </>
   )
 }
