63eff70f5555b55e92c57ef71a7b897ccbe44781
Agrim <agrimsharma1714@gmail.com>
2023-06-30 22:35:08 +0530

    sidebar added

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,14 @@ import DesktopSidebar from '@/components/DesktopSidebar'
 import '@/styles/globals.css'
 import "../sass/_em-artistProfile.module.scss";
 import Sidebar from '@/components/Sidebar';
+import Desktop from '@/components/Desktop';
 
 export default function App({ Component, pageProps }) {
   return (
     <div className='flex'>
-        <Sidebar />
+        {/* <Sidebar /> */}
         {/* <DesktopSidebar /> */}
+        <Desktop />
       <Component {...pageProps} />
     </div>
   )
