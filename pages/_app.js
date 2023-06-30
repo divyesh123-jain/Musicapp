8f4201276968b88ee37aff6dacd75012c788052a
divyesh123-jain <97737499+divyesh123-jain@users.noreply.github.com>
2023-06-30 18:20:25 +0530

    upadte

--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,8 @@ import Sidebar from '@/components/Sidebar';
 export default function App({ Component, pageProps }) {
   return (
     <div className='flex'>
-        <Sidebar />
-        {/* <DesktopSidebar /> */}
+        {/* <Sidebar /> */}
+        <DesktopSidebar />
       <Component {...pageProps} />
     </div>
   )
