69faec9cb4e32cc013a0b950ac5b9e737cc515aa
Agrim <agrimsharma1714@gmail.com>
2023-07-12 22:40:11 +0530

    made artists.js in api folder

--- pages/artists.js
+++ pages/artists.js
@@ -1,3 +1,45 @@
+
+//! below is taking data from api folder
+
+// import ArtistCard from "@/components/Artists/ArtistCard";
+// import SearchBar from "@/components/Artists/SearchBar";
+// import React, { useEffect, useState } from "react";
+// import { BsArrowLeftCircle } from "react-icons/bs";
+// import axios from "axios";
+
+// const Artists = ({ data }) => {
+//   return (
+//     <>
+//       <div className="md:ml-[270px] min-h-[100vh] text-white p-5">
+//         <div className="align-items-center justify-content-between flex text-white">
+//           <h2 className="text-white ml-[10px] mt-[52px] font-sf-pro-text text-4xl font-semibold tracking-normal text-left ">
+//             All Artists
+//           </h2>
+//         </div>
+
+//         <SearchBar />
+//         <div className="flex justify-center items-center">
+//           <div className="sm:mb-0 mt-10 grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center lg:grid-cols-4 md:w-full md:space-x-2">
+//             {data &&
+//               data.length > 0 &&
+//               data.map((item) => (
+//                 <ArtistCard
+//                   key={item.id}
+//                   name={item.full_name}
+//                   profileImage="https://via.placeholder.com/112x112"
+//                 />
+//               ))}
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Artists;
+
+// !below with serverside props
+
 import ArtistCard from "@/components/Artists/ArtistCard";
 import SearchBar from "@/components/Artists/SearchBar";
 import React from "react";
@@ -52,11 +94,7 @@ export async function getServerSideProps() {
 
 export default Artists;
 
-
-
-
-
-
+// !below without serverside props
 
 // import ArtistCard from "@/components/Artists/ArtistCard";
 // import SearchBar from "@/components/Artists/SearchBar";
@@ -112,10 +150,3 @@ export default Artists;
 // };
 
 // export default Artists;
-
-
-
-
-
-
-
