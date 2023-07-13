e21493d388d89048cb9b0fd3a429834862316a4d
divyesh123-jain <97737499+divyesh123-jain@users.noreply.github.com>
2023-07-13 18:30:16 +0530

    update

--- components/Artists/ArtistCard.js
+++ components/Artists/ArtistCard.js
@@ -2,13 +2,17 @@ import React from "react";
 import { useRouter } from "next/router";
 
 const ArtistCard = ({ data, profileImage }) => {
-  const { full_name, username } = data;
-
   const router = useRouter();
 
   const handleArtistClick = () => {
-    router.push(`/artists/${username}`);
+    router.push(`/artists/${data.username}`);
   };
+
+  if (!data || !data.full_name) {
+    return null; // or you can render an error message or placeholder component
+  }
+
+  const { full_name, username } = data;
 
   return (
     <div
@@ -19,7 +23,7 @@ const ArtistCard = ({ data, profileImage }) => {
         className="w-[112px] h-[112px] mx-10 justify-center object-cover object-center rounded-full"
         href="/ArtistProfile"
         src={profileImage}
-        alt={full_name} // Ensure that `full_name` is properly destructured
+        alt={full_name}
       />
 
       <div className="bottom-4 left-3 relative">
@@ -37,50 +41,3 @@ const ArtistCard = ({ data, profileImage }) => {
 };
 
 export default ArtistCard;
-
-
-
-
-
-
-
-// import React from "react";
-// import { useRouter } from "next/router";
-
-// const ArtistCard = ({ data, profileImage }) => {
-//   const { full_name, username  } = data;
-
-//   const router = useRouter();
-//   return (
-//     <div className="relative group w-[214.8px] h-[207.8px] border-[1px] justify-center items-center sm:py-12 px-4 flex flex-col space-y-2 cursor-pointer rounded-xl backdrop-opacity-25 bg-white/10 border-white/40 hover:smooth-hover hover:backdrop-opacity-25 hover:bg-black/20 hover:border-red-500"
-//     onClick={() => {
-//             router.push(`/artists/${username}`);
-//           }}>
-//       <img
-//         className="w-[112px] h-[112px] mx-10 justify-center object-cover object-center rounded-full"
-//         href="/ArtistProfile"
-//         src={profileImage}
-//         alt={full_name}
-//       />
-
-//       <div className="bottom-4 left-3 relative">
-//         <div className="w-[78px] h-[37px] pb-2 top-0 absolute">
-//           <div className="left-1 top-[15px] absolute text-white text-[16px] font-normal">
-//             {full_name}
-//           </div>
-//           <div className="left-[41px] top-0 pb-3 absolute text-stone-500 text-[12px] font-normal">
-//             Profile
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ArtistCard;
-
-
-
-
-
-
--- pages/artists.js
+++ pages/artists.js
@@ -1,45 +1,3 @@
-
-//! below is taking data from api folder
-
-// import ArtistCard from "@/components/Artists/ArtistCard";
-// import SearchBar from "@/components/Artists/SearchBar";
-// import React, { useEffect, useState } from "react";
-// import { BsArrowLeftCircle } from "react-icons/bs";
-// import axios from "axios";
-
-// const Artists = ({ data }) => {
-//   return (
-//     <>
-//       <div className="md:ml-[270px] min-h-[100vh] text-white p-5">
-//         <div className="align-items-center justify-content-between flex text-white">
-//           <h2 className="text-white ml-[10px] mt-[52px] font-sf-pro-text text-4xl font-semibold tracking-normal text-left ">
-//             All Artists
-//           </h2>
-//         </div>
-
-//         <SearchBar />
-//         <div className="flex justify-center items-center">
-//           <div className="sm:mb-0 mt-10 grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center lg:grid-cols-4 md:w-full md:space-x-2">
-//             {data &&
-//               data.length > 0 &&
-//               data.map((item) => (
-//                 <ArtistCard
-//                   key={item.id}
-//                   name={item.full_name}
-//                   profileImage="https://via.placeholder.com/112x112"
-//                 />
-//               ))}
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Artists;
-
-// !below with serverside props
-
 import ArtistCard from "@/components/Artists/ArtistCard";
 import SearchBar from "@/components/Artists/SearchBar";
 import React from "react";
@@ -59,7 +17,7 @@ const Artists = ({ data }) => {
         <SearchBar />
         <div className="flex justify-center items-center">
           <div className="sm:mb-0 mt-10 grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center lg:grid-cols-4 md:w-full md:space-x-2">
-            {data.map((item) => (
+            {data?.map((item) => (
               <ArtistCard
                 key={item.id}
                 // name={item.full_name}
@@ -77,8 +35,9 @@ const Artists = ({ data }) => {
 
 export async function getServerSideProps() {
   try {
-    const response = await axios.get(process.env.API_URL);
+    const response = await axios.get(process.env.NEXT_PUBLIC_API_URL);
     const data = response.data.data;
+    console.log(data)
     return {
       props: {
         data,
@@ -95,60 +54,3 @@ export async function getServerSideProps() {
 }
 
 export default Artists;
-
-// !below without serverside props
-
-// import ArtistCard from "@/components/Artists/ArtistCard";
-// import SearchBar from "@/components/Artists/SearchBar";
-// import { useRouter } from "next/router";
-// import React, { useEffect, useState } from "react";
-// import { BsArrowLeftCircle } from "react-icons/bs";
-// import axios from "axios";
-
-// const Artists = () => {
-//   const [data, setData] = useState([]);
-//   const router = useRouter();
-
-//   const fetchAPI = () => {
-//     axios
-//       .get("link")
-//       .then((res) => {
-//         // console.log(res.data);
-//         setData(res.data.data);
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching data:", error);
-//       });
-//   };
-
-//   useEffect(() => {
-//     fetchAPI();
-//   }, []);
-
-//   return (
-//     <>
-//       <div className="md:ml-[270px] min-h-[100vh] text-white p-5">
-//         <div className="align-items-center justify-content-between flex text-white">
-//           <h2 className="text-white ml-[10px]  mt-[52px] font-sf-pro-text text-4xl font-semibold tracking-normal text-left ">
-//             All Artists
-//           </h2>
-//         </div>
-
-//         <SearchBar />
-//         <div className="flex justify-center items-center">
-//           <div className="sm:mb-0 mt-10 grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 items-center lg:grid-cols-4 md:w-full md:space-x-2">
-//             {/* <ArtistCard
-//               name="Ari Elkins"
-//               profileImage="https://via.placeholder.com/112x112"
-//             /> */}
-//             {data.map((item) => (
-//         <ArtistCard key={item.id} name={item.full_name} profileImage="https://via.placeholder.com/112x112" />
-//       ))}
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// };
-
-// export default Artists;
